e28051d8aecc79fa69d518226d3499e4abc8d25a
Codyjamesie <codyjhaug@gmail.com>
2022-01-12 21:12:12 -0800

    First React project, updates

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css';
 
 class App extends Component {
@@ -28,7 +29,7 @@ class App extends Component {
     render() {    
         const { robots, searchfield } = this.state
         const filteredRobots = robots.filter(robot =>{ 
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robot.name.toLowerCase().startsWith(searchfield.toLowerCase())
     })
         return !robots.length ?
             <h1>Loading</h1>:
@@ -37,7 +38,9 @@ class App extends Component {
                 <h1 className='f2'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
+                    <ErrorBoundary>
                     <CardList robots={filteredRobots}/>
+                    </ErrorBoundary>
                 </Scroll>
                 </div>
           );
